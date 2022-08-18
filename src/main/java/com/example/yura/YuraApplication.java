2d81cfaeecb8f7371d12c6e458ccd0c48a0998be
Khrenov Alex <a.hrenov@egartech.com>
2022-08-18 15:42:07 +0400

    Initial commit

---
+++ src/main/java/com/example/yura/YuraApplication.java
@@ -0,0 +1,14 @@
+package com.example.yura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YuraApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Yura");
+		SpringApplication.run(YuraApplication.class, args);
+	}
+
+}
